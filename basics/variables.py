309989a171b0ef9cba50c6e69241ad9a4c210ebf
Ekaterina <178106357+Kate-qa31@users.noreply.github.com>
2026-07-19 00:23:06 +0500

    Add variables and arithmetic exercises

---
+++ basics/variables.py
@@ -0,0 +1,69 @@
+# Task 1
+movie_name = "Harry Potter and the Sorcerer's Stone"
+print(f"Название фильма: {movie_name}")
+year_published = 2001
+print(f"Год выпуска: {year_published}")
+kinopoisk_rate = 8.3
+print(f"Рейтинг на Кинопоиске: {kinopoisk_rate}")
+main_actors = ["Daniel Radcliffe", "Rupert Grint", "Emma Watson"]
+print(f"Главные актёры: {main_actors}")
+
+# Task 2
+name = "Tom"
+print(name)
+name = "Bob"
+print(f"Изменили имя на: {name}")
+
+# Task 3
+a = b = c = 10
+print(f"До изменения a = {a}")
+print(f"До изменения b = {b}")
+print(f"До изменения c = {c}")
+c += 5
+print(f"После изменения a = {a}")
+print(f"После изменения b = {b}")
+print(f"После изменения c = {c}")
+
+# Task 4
+a, b, c = "Tom", "Bob", "Jake"
+print(f"a = {a}")
+print(f"b = {b}")
+print(f"c = {c}")
+
+# Task 5
+a = 10
+b = 5.5
+sum_result = a + b
+print(sum_result)
+sub_result = a - b
+print(sub_result)
+mul_result = a * b
+print(mul_result)
+div_result = a / b
+print(div_result)
+pow_result = a ** b
+print(pow_result)
+floor_result = a // b
+print(floor_result)
+mod_result = a % b
+print(mod_result)
+
+# Task 6
+c = 6
+c += 3
+print(c)
+c -= 2
+print(c)
+c /= 2
+print(c)
+c *= 4
+print(c)
+
+# Task 7
+name = "Python"
+x = "Hello"
+print(x + name)
+print(name * 3)
+y = 10
+z = "10"
+print(y + int(z))
